c0d5badb81f2ea0bb035d862813792486dbebea3
maihuysl1999 <maihuysl1999@gmail.com>
2021-04-08 03:38:48 +0000

    Change todo status

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,11 +4,22 @@
 　・チェックボックスにチェックが入っているか管理する
 　・チェックボックスにチェックが入っているかアイテムをグレーアウトする
 */
+import {useState} from 'react';
+
 function TodoItem( {item} ) {
+  const [isBlack, setIsBlack] = useState(true);
+
+  const handleChnageTextColor = (e) => {
+    setIsBlack(!isBlack);
+    
+  }
   return (
     <label className="panel-block">
-      <input type="checkbox" />
-      {item.text}
+      <input type="checkbox"
+      onchange ="toggleCheck" 
+      value={isBlack} onChange={handleChnageTextColor} 
+      />
+      <div className={!isBlack ? 'has-text-grey-light' : ''}>{item.text}</div>
     </label>
   );
 }
